49d45279d9399d12cdd63eb7780b4f9a3fe9bdbf
Clever dos Anjos <clever@clever.com.br>
2018-11-12 09:32:46 -0200

    improvement
    The script now checks S3 first before exporting the app

--- UploadQlikS3.ps1
+++ UploadQlikS3.ps1
@@ -24,20 +24,25 @@ $apps = Get-QlikApp -filter "Published eq true and tags.name eq 'S3'" -full
 foreach ($app in $apps) {
     try {
         Write-Verbose "Checking if $($app.name) is newer" 
-        $f = (Get-ChildItem -filter "$($app.id).qvf")
-        if (!$f -or $f.LastWriteTime.DateTime -lt $app.modifiedDate.ToDateTime ) {
+        $s3 = Get-S3Object `
+            -Region $region `
+            -BucketName $bucketName `
+            -Key "$($app.id).qvf" `
+            -AccessKey $credentials.'Access key ID' `
+            -SecretKey $credentials.'Secret access key'
+
+        if (!$s3 -or ( $s3.LastModified.ToString("yyyy/MM/dd hh:mm") -lt $app.modifiedDate ) ) {
             Write-Verbose "Dumping $($app.name)"
-            Export-QlikApp -id $app.id 
-            
+            Export-QlikApp -id $app.id            
             Write-Verbose "Tagging to enable management" 
-            $owner = $a.owner.userDirectory + "\" + $a.owner.userId
+
             $tags = @(`
-                @{key = "name"; value = $app.name}, `
+                @{key = "name"; value = $( $app.name -replace '[()]', '')}, `
                 @{key = "modifiedDate"; value = $app.modifiedDate}, `
                 @{key = "stream"; value = $app.stream.name}, `
-                @{key = "userDirectory"; value = $a.owner.userDirectory}, ` 
-                @{key = "userId"; value = $a.owner.userId })
-
+                @{key = "userDirectory"; value = $(if ($a.owner.userDirectory.GetType() -eq "string") {$a.owner.userDirectory } else {$a.owner.userDirectory[0].Trim()} )}, ` 
+                @{key = "userId"; value = $(if ($a.owner.userId.GetType() -eq "string") {$a.owner.userId } else {$a.owner.userId[0].Trim()})})
+            $tags
             Write-Verbose "Uploadind as $($app.id).qvf"    
             Write-S3Object `
                 -Region $region `
@@ -46,17 +51,12 @@ foreach ($app in $apps) {
                 -AccessKey $credentials.'Access key ID' `
                 -SecretKey $credentials.'Secret access key' `
                 -TagSet $tags
-            Write-Verbose "Checking"
-            Get-S3Object `
-                -Region $region `
-                -BucketName $bucketName `
-                -Key "$($app.id).qvf" `
-                -AccessKey $credentials.'Access key ID' `
-                -SecretKey $credentials.'Secret access key'
         }
     }
     catch {
         Write-Error $_.Exception.Message
+        Write-Error $app.name
     }
 }
+#Remove-Item *
 Pop-Location
